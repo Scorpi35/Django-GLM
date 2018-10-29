76ef9020210501416dd0756daac50aa309863348
Ashish Subedi <subediashish35@gmail.com>
2018-10-29 21:44:39 +0545

    Updated

---
+++ PersonalBlog/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url, include
+
+urlpatterns = [
+    url(r'^HomePage/', include('HomePage.urls'), name='HomePage'),
+    url(r'^Skills/', include('Skills.urls'), name='Skills')
+]
+
