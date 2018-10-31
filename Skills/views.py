b406279deded01fe1be6ee1e1293ddd31c81176d
Ashish Subedi <subediashish35@gmail.com>
2018-10-31 18:13:00 +0545

    Updated

---
+++ Skills/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+# Returning views
+
+def index(request):
+
+    template = loader.get_template('Skills/index.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context, request))
+
