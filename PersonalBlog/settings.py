66a40d7c54e0050a8b5fd76cbe4854cc33d6d849
Ashish Subedi <subediashish35@gmail.com>
2018-10-31 18:13:52 +0545

    Update settings.py

--- PersonalBlog/settings.py
+++ PersonalBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lgfs+drj4veo$ic_z(t5_q_#$(1#$nzstqf5w1k9l8t$8cgtzt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.101', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.0.104', '127.0.0.1']
 
 
 # Application definition
